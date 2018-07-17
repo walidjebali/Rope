13cc84077c368c60d1bd0ac931d5cc166eb3de16
Walid <jebaliiwalid@gmail.com>
2018-07-17 14:27:24 +0100

    Rope

---
+++ Rope.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+	void Start () {
+		
+	}
+		void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			GetComponent<DistanceJoint2D> ().enabled = true;
+			GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 500f);
+			gameObject.transform.position= new Vector3 (transform.position.x,-1,transform.position.z);
+			StartCoroutine (wait ());
+		}
+	}
+	IEnumerator wait(){
+		yield return new WaitForSeconds (3);
+		GetComponent<DistanceJoint2D> ().enabled = false;
+
+	
+	}
+}
